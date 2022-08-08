77df2a4cd1b80ea3ec2de02bd50e36c6a79e8fd9
Joshua Landwehr <snapcore@gmail.com>
2022-08-08 15:37:28 -0700

    Add workflow to run some notebook tests.

---
+++ test/test_nyctaxi_rideshare.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*- --------------------------------------------------===#
+#
+#  Copyright 2022 Trovares Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===----------------------------------------------------------------------===#
+
+import os
+import sys
+import unittest
+from io import StringIO
+from util.util import run_notebook
+
+class TestJupyterNotebook(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        input = u"""2020
+                    6
+                 """
+        real_path = os.path.realpath(__file__)
+        dir_path = os.path.dirname(real_path)
+        notebook = 'nyctaxi_rideshare.ipynb'
+        notebook_loc = dir_path + '/../demos/nyctaxi/'
+        sys.path.insert(0, notebook_loc)
+        # Pass stdin input to the notebook.
+        sys.stdin = StringIO(input)
+        cls.nb, errors = run_notebook(notebook_loc + notebook)
+
+    def test_notebook_runs(self):
+        # For now just test the notebook runs.
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
