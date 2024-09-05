8d7743e65da784a3e288fbf8dde082f07773e27c
Joshua Landwehr <josh@trovares.com>
2024-09-05 12:50:25 -0700

    Fix workflow and update copyrights.

--- AWS/launch_utils.py
+++ AWS/launch_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- --------------------------------------------------===#
 #
-#  Copyright 2021 Trovares Inc.
+#  Copyright 2021-2024 Trovares Inc.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
